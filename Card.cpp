ebe41a89cdc4f39cea1cec934284b7e0449cf44d
Samuel-Schreiner <60583060+Samuel-Schreiner@users.noreply.github.com>
2020-03-20 15:12:51 -0500

    Add files via upload

--- Card.cpp
+++ Card.cpp
@@ -1,1 +1,31 @@
 #include "Card.h"
+
+Card::Card()
+{
+	
+}
+
+int Card::getcValue()
+{
+	return 0;
+}
+
+void Card::setcValue()
+{
+}
+
+int Card::getcSuit()
+{
+}
+
+void Card::setcSuit()
+{
+}
+
+void Card::getcImg()
+{
+}
+
+void Card::setcImg()
+{
+}
--- Client.cpp
+++ Client.cpp
@@ -1,2 +1,18 @@
 #include "Client.h"
 // #include "Player.h"
+
+Client::Client()
+{
+}
+
+int Client::getIDno
+{
+}
+
+int Client::getName()
+{
+}
+
+void Client::setName()
+{
+}
