aca4f91b1178f614b479385c3f43781077f889dc
Samuel-Schreiner <60583060+Samuel-Schreiner@users.noreply.github.com>
2020-03-20 15:11:31 -0500

    Add files via upload

---
+++ Player.cpp
@@ -0,0 +1,29 @@
+#include "Player.h"
+
+Player::Player()
+{
+}
+
+Card::Card Player::getCards()
+{
+}
+
+void Player::setCards()
+{
+}
+
+int Player::getMoney()
+{
+}
+
+void Player::setMoney()
+{
+}
+
+int Player::getBet()
+{
+}
+
+void Player::setBet()
+{
+}
